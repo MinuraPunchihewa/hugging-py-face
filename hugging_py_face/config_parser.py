9e019215f9efe30320facc1c962dfe86c17942c6
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-14 00:15:21 +0530

    included a default path to the config file in ConfigParser

--- hugging_py_face/config_parser.py
+++ hugging_py_face/config_parser.py
@@ -1,9 +1,10 @@
+import os
 import yaml
 
 
 class ConfigParser:
-    def __init__(self, file_path):
-        with open(file_path, 'r') as f:
+    def __init__(self, file_path='config/config.yaml'):
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), file_path, 'r')) as f:
             self.config_dict = yaml.safe_load(f)
 
     def get_config_dict(self):
