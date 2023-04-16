edf835ace03b94da8b57bdb99b7de7b939b60d04
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-16 11:09:24 +0530

    added support for the audio classification task

--- hugging_py_face/audio_processing.py
+++ hugging_py_face/audio_processing.py
@@ -1,4 +1,4 @@
-from typing import Text, Dict, Optional
+from typing import Text, List, Dict, Optional
 from .multimedia_processing import MultimediaProcessing
 
 
@@ -15,3 +15,13 @@ class AudioProcessing(MultimediaProcessing):
         :return: the text transcription of the audio file
         """
         return self._query(input, model=model, task="speech-recognition")
+
+    def audio_classification(self, input: Text, model: Optional[Text] = None) -> List:
+        """
+        Classify an audio file from a file path or an url.
+
+        :param input: the file path or url to the audio file to classify
+        :param model: the model to use for the audio classification task. If not provided, the recommended model from Hugging Face will be used.
+        :return: a list containing the labels and the confidence score for each label
+        """
+        return self._query(input, model=model, task="audio-classification")
