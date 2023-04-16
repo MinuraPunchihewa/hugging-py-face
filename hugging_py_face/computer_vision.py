cd9da5bec7d819c4c103b6d786deef3389dc332f
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-16 10:40:24 +0530

    added docstrings for the ComputerVision tasks

--- hugging_py_face/computer_vision.py
+++ hugging_py_face/computer_vision.py
@@ -10,16 +10,16 @@ class ComputerVision(MultimediaProcessing):
 
     def image_classification(self, input: Text, model: Optional[Text] = None) -> List:
         """
-        :param input:
-        :param model:
-        :return:
+        :param input: the file path or url to the image to classify
+        :param model: the model to use for the image classification task. If not provided, the recommended model from Hugging Face will be used.
+        :return: a list of dictionaries each containing the label and the confidence score for that label
         """
         return self._query(input, model=model, task="image-classification")
 
     def object_detection(self, input: Text, model: Optional[Text] = None) -> List:
         """
-        :param input:
-        :param model:
-        :return:
+        :param input: the file path or url to the image to perform object detection on
+        :param model: the model to use for the object detection task. If not provided, the recommended model from Hugging Face will be used.
+        :return: a list of dictionaries each containing the label, the confidence score for that label, and the bounding box coordinates
         """
         return self._query(input, model=model, task="object-detection")
