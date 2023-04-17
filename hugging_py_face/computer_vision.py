2b124c4daec24d5961bc292c17d15d1ac31f8eee
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-17 22:02:20 +0530

    added support for list type inputs for the ComputerVision tasks

--- hugging_py_face/computer_vision.py
+++ hugging_py_face/computer_vision.py
@@ -1,4 +1,4 @@
-from typing import Text, List, Optional
+from typing import Text, List, Optional, Union
 from .multimedia_processing import MultimediaProcessing
 
 
@@ -6,22 +6,28 @@ class ComputerVision(MultimediaProcessing):
     def __init__(self, api_token):
         super().__init__(api_token)
 
-    def image_classification(self, input: Text, model: Optional[Text] = None) -> List:
+    def image_classification(self, inputs: Union[Text, List], model: Optional[Text] = None) -> List:
         """
         Classify an image from a file path or an url.
 
-        :param input: the file path or url to the image to classify
+        :param inputs: a string or a list of strings of the file paths or urls of the images to classify
         :param model: the model to use for the image classification task. If not provided, the recommended model from Hugging Face will be used.
         :return: a list of dictionaries each containing the label and the confidence score for that label
         """
-        return self._query(input, model=model, task="image-classification")
+        if type(input) == list:
+            return self._query_in_list(inputs, model=model, task="image-classification")
+        elif type(input) == str:
+            return self._query(inputs, model=model, task="image-classification")
 
-    def object_detection(self, input: Text, model: Optional[Text] = None) -> List:
+    def object_detection(self, inputs: Union[Text, List], model: Optional[Text] = None) -> List:
         """
         Perform object detection on an image from a file path or an url.
 
-        :param input: the file path or url to the image to perform object detection on
+        :param inputs: a string or a list of strings of the file paths or urls of the images to perform object detection on
         :param model: the model to use for the object detection task. If not provided, the recommended model from Hugging Face will be used.
         :return: a list of dictionaries each containing the label, the confidence score for that label, and the bounding box coordinates
         """
-        return self._query(input, model=model, task="object-detection")
+        if type(input) == list:
+            return self._query(inputs, model=model, task="object-detection")
+        elif type(input) == str:
+            return self._query(inputs, model=model, task="object-detection")
