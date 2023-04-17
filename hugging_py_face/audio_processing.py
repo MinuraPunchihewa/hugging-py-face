2ac145f4f483774aaaed840d0b3ce255a531c1da
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-17 23:57:05 +0530

    added support for the speech recognition and audio classification tasks to be applied on DataFrames

--- hugging_py_face/audio_processing.py
+++ hugging_py_face/audio_processing.py
@@ -1,4 +1,6 @@
+from pandas import DataFrame
 from typing import Text, List, Dict, Optional, Union
+
 from .multimedia_processing import MultimediaProcessing
 
 
@@ -19,6 +21,19 @@ class AudioProcessing(MultimediaProcessing):
         elif type(input) == str:
             return self._query(input, model=model, task="speech-recognition")
 
+    def speech_recognition_in_df(self, df: DataFrame, column: Text, model: Optional[Text] = None) -> DataFrame:
+        """
+        Perform speech recognition on audio files from a DataFrame.
+
+        :param df: a pandas DataFrame containing the audio files to perform speech recognition on.
+        :param column: the name of the column containing the file paths or urls of the audio files to perform speech recognition on.
+        :param model: the model to use for the speech recognition task. If not provided, the recommended model from Hugging Face will be used.
+        :return: a pandas DataFrame with the text recognized from the audio files. The text will be added as a new column called 'predictions' to the original DataFrame.
+        """
+        predictions = self._query_in_df(df, column, model=model, task="speech-recognition")
+        df["predictions"] = [prediction['text'] for prediction in predictions]
+        return df
+
     def audio_classification(self, inputs: Text, model: Optional[Text] = None) -> List:
         """
         Classify an audio file from a file path or an url.
@@ -31,3 +46,16 @@ class AudioProcessing(MultimediaProcessing):
             return self._query_in_list(inputs, model=model, task="audio-classification")
         elif type(input) == str:
             return self._query(input, model=model, task="audio-classification")
+
+    def audio_classification_in_df(self, df: DataFrame, column: Text, model: Optional[Text] = None) -> DataFrame:
+        """
+        Classify audio files from a DataFrame.
+
+        :param df: a pandas DataFrame containing the audio files to classify.
+        :param column: the name of the column containing the file paths or urls of the audio files to classify.
+        :param model: the model to use for the audio classification task. If not provided, the recommended model from Hugging Face will be used.
+        :return: a pandas DataFrame with the label for the audio files. Each label added will be the one with the highest confidence score for that particular audio file. The label will be added as a new column called 'predictions' to the original DataFrame.
+        """
+        predictions = self._query_in_df(df, column, model=model, task="audio-classification")
+        df["predictions"] = [prediction[0]['label'] for prediction in predictions]
+        return df
