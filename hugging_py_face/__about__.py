2915a1c5a3695b728d7b521bf34bdb4108214bd8
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-18 22:27:04 +0530

    updated the package version

--- hugging_py_face/__about__.py
+++ hugging_py_face/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'hugging_py_face'
 __package_name__ = 'hugging_py_face'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __description__ = "Hugging-Py-Face, the Python client for the Hugging Face Inference API."
 __email__ = "minurapunchihewa17@gmail.com"
 __author__ = 'Minura Punchihewa'
