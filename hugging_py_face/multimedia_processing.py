d5cb3154de89e4de43cbde0530c66e20c29f6c6b
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-28 16:11:03 +0530

    added logging and a retry mechanism to MultimediaProcessing

--- hugging_py_face/multimedia_processing.py
+++ hugging_py_face/multimedia_processing.py
@@ -1,7 +1,16 @@
 import json
+import time
+import logging
 import requests
+import logging.config
 from typing import Text, Dict, List, Optional, Union
+
 from .config_parser import ConfigParser
+from .exceptions import HTTPServiceUnavailableException
+
+logging_config_parser = ConfigParser('config/logging.yaml')
+logging.config.dictConfig(logging_config_parser.get_config_dict())
+logger = logging.getLogger()
 
 
 class MultimediaProcessing:
@@ -10,6 +19,8 @@ class MultimediaProcessing:
 
         config_parser = ConfigParser()
         self.config = config_parser.get_config_dict()
+
+        self.logger = logger
 
     def _query(self, input: Text, model: Optional[Text] = None, task: Optional[Text] = None) -> Union[Dict, List]:
         api_url = f"{self.config['BASE_URL']}/{model if model is not None else self.config['TASK_MODEL_MAP'][task]}"
@@ -28,8 +39,23 @@ class MultimediaProcessing:
             with open(input, "rb") as f:
                 data = f.read()
 
+        retries = 0
+
+        while retries < self.config['MAX_RETRIES']:
+            retries += 1
+
         response = requests.request("POST", api_url, headers=headers, data=data)
-        return json.loads(response.content.decode("utf-8"))
+        if response.status_code == int(self.config['HTTP_SERVICE_UNAVAILABLE']):
+            self.logger.info(f"Status code: {response.status_code}.")
+            self.logger.info("Retrying..")
+            time.sleep(1)
+        else:
+            return json.loads(response.content.decode("utf-8"))
+
+        self.logger.info(f"Status code: {response.status_code}.")
+        self.logger.info("Connection to the server failed after reaching maximum retry attempts.")
+        self.logger.debug(f"Response: {json.loads(response.content.decode('utf-8'))}.")
+        raise HTTPServiceUnavailableException("The HTTP service is unavailable.")
 
     def _query_in_list(self, inputs: List[Text], model: Optional[Text] = None, task: Optional[Text] = None) -> List[Union[Dict, List]]:
         return [self._query(input, model, task) for input in inputs]
