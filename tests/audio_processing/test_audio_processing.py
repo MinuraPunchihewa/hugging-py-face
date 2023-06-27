092b9e8d6ff5c0586f8a61d01a9171ef271d1257
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-27 22:39:46 +0530

    updated the unit tests for AudioProcessing to check for approximate values

--- tests/audio_processing/test_audio_processing.py
+++ tests/audio_processing/test_audio_processing.py
@@ -14,6 +14,12 @@ class TestAudioProcessing(unittest.TestCase):
         cls.ap = AudioProcessing(os.environ.get("API_KEY"))
         cls.inputs = os.path.join(os.path.dirname(__file__), '..', 'resources', 'amused.wav')
 
+    def assertAlmostEqualList(self, expected, actual, places=7):
+        self.assertEqual(len(expected), len(actual))
+        for exp, act in zip(expected, actual):
+            self.assertEqual(exp['label'], act['label']), "Label values are not equal."
+            self.assertAlmostEqual(exp['score'], act['score'], places=places), "Score values are not approximately equal."
+
     def test_automatic_speech_recognition(self):
         try:
             self.assertEqual(
@@ -27,14 +33,14 @@ class TestAudioProcessing(unittest.TestCase):
 
     def test_audio_classification(self):
         try:
-            self.assertEqual(
-                self.ap.audio_classification(self.inputs),
-                [
-                    {'label': 'hap', 'score': 0.996896505355835},
-                    {'label': 'sad', 'score': 0.002958094235509634},
-                    {'label': 'neu', 'score': 9.905487240757793e-05},
-                    {'label': 'ang', 'score': 4.624627763405442e-05}
-                ],
-            )
+            expected_result = [
+                {'label': 'hap', 'score': 0.996896505355835},
+                {'label': 'sad', 'score': 0.002958094235509634},
+                {'label': 'neu', 'score': 9.905487240757793e-05},
+                {'label': 'ang', 'score': 4.624627763405442e-05}
+            ]
+
+            actual_result = self.ap.audio_classification(self.inputs)
+            self.assertAlmostEqualList(expected_result, actual_result)
         except HTTPServiceUnavailableException:
             self.assertRaises(HTTPServiceUnavailableException, lambda: self.ap.audio_classification(self.inputs))
