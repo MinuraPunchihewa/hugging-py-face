a28aed91635a0c2413a2fd2273e961b5c869804f
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-07-07 23:17:11 +0530

    added a function to get a list of in_df supported tasks

--- hugging_py_face/__init__.py
+++ hugging_py_face/__init__.py
@@ -1,3 +1,5 @@
+import inspect
+
 from .nlp import NLP
 from .computer_vision import ComputerVision
 from .audio_processing import AudioProcessing
@@ -10,3 +12,11 @@ def get_supported_tasks():
     config = config_parser.get_config_dict()
 
     return list(config['TASK_MODEL_MAP'].keys())
+
+
+def get_in_df_supported_tasks():
+    tasks = []
+    for task_family in [NLP, ComputerVision, AudioProcessing]:
+        tasks += [func[0].replace('_in_df', '').replace('_', '-') for func in inspect.getmembers(task_family, predicate=inspect.isfunction) if func[0].endswith('_in_df') and 'query' not in func[0]]
+
+    return tasks
